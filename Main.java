0e8d1cc9c62c6e7e4f93d851e6f51112095752b9
fernandoricciomartinez-hash <fernandoricciomartinez@gmail.com>
2026-03-20 02:27:12 +0100

    Add method to show current date and time
    Added a method to display the current date and time.

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDateTime;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        mostrarFechaHora();
+    }
+
+    public static void mostrarFechaHora() {
+        System.out.println("Fecha y hora actual: " + LocalDateTime.now());
     }
 }
